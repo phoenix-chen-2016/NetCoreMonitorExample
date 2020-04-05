debf33861689910f8d822f5f3e89fb841ada8ebd
Phoenix <fphoenix923@hotmail.com>
2020-04-05 17:26:40 +0800

    刪除多餘的程式

--- SampleWeb/SampleEventSource.cs
+++ SampleWeb/SampleEventSource.cs
@@ -1,32 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.Tracing;
 
 namespace SampleWeb
 {
 	public class SampleEventSource : EventSource
 	{
-		private Dictionary<string, DiagnosticCounter> _counters = new Dictionary<string, DiagnosticCounter>();
-
 		public SampleEventSource() : base("Sample")
 		{
-		}
-
-		public void AddPollingCounterDef(
-			string key,
-			string displayName,
-			Func<double> merticProvider,
-			string displayUnits = null)
-		{
-			_counters.Add(
-				key,
-				new PollingCounter(key, this, merticProvider)
-				{
-					DisplayName = displayName,
-					DisplayUnits = displayUnits
-				});
 		}
 	}
 }
